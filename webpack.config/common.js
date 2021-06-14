308c3ebf42c0675a9471dce2fd747ca639334b66
david.zheng <280541464@qq.com>
2021-06-14 23:22:31 +0800

    修改 webpack.config 下的 common.js 文件

--- webpack.config/common.js
+++ webpack.config/common.js
@@ -29,6 +29,7 @@ module.exports = {
       {
         test: /\.js$/,
         use: ['babel-loader'],
+        include: path.resolve(__dirname, 'src'),
         exclude: path.resolve(__dirname, '../node_modules'),
       },
       {
@@ -46,6 +47,7 @@ module.exports = {
           'postcss-loader',
           'sass-loader',
         ],
+        include: path.resolve(__dirname, 'src'),
         exclude: path.resolve(__dirname, '../node_modules'),
       },
     ],
